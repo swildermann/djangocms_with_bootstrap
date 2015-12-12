a6d8911545aff1a6aac1a3662ab186488dcb555a
Sven Wildermann <git@wildermannonline.de>
2015-12-12 22:42:23 +0100

    changed settings (no if/else statement necessary)

--- webpage/settings.py
+++ webpage/settings.py
@@ -211,28 +211,16 @@ CMS_PLACEHOLDER_CONF = {
         },
 }
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'HOST': 'localhost',
-            'NAME': 'project.db',
-            'PASSWORD': '',
-            'PORT': '',
-            'USER': ''
-        }
-    }
+}
 
 
 
